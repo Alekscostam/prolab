8f6310df808a8a17e429832476d955218bc7b152
bartosz.lukaszewski <bartosz.lukaszewski@primigenius.pl>
2021-08-06 10:28:39 +0200

    fix wyszukiwajki

--- ui/src/containers/layout/Sidebar.js
+++ ui/src/containers/layout/Sidebar.js
@@ -94,20 +94,29 @@ class Sidebar extends React.Component {
     }
 
     handleFilter(filterValue) {
-        console.log('handleFilter',filterValue);
+        console.log('handleFilter', filterValue);
         const menu = this.state.menu;
         if (menu !== undefined && filterValue !== null) {
             if (filterValue === undefined || filterValue === null || filterValue === '') {
-                this.setState({ filteredMenu: menu, filterValue });
+                console.log('handleFilter 1', filterValue);
+                this.setState({ filteredMenu: menu, filterValue }, () => {
+                    console.log('afterset 1', this.state.filterValue);
+                });
             } else {
                 let filteredMenu = [];
                 menu.forEach((item) => {
                     this.processItem(item, filteredMenu, filterValue);
                 });
-                this.setState({ filteredMenu, filterValue });
+                console.log('handleFilter 2', filterValue);
+                this.setState({ filteredMenu, filterValue }, () => {
+                    console.log('afterset 2', this.state.filterValue);
+                });
             }
         } else {
-            this.setState({ filteredMenu: [], filterValue });
+            console.log('handleFilter 3', filterValue);
+            this.setState({ filteredMenu: [], filterValue }, () => {
+                console.log('afterset 3', this.state.filterValue);
+            });
         }
     }
 
@@ -137,6 +146,7 @@ class Sidebar extends React.Component {
     }
 
     render() {
+        const { collapsed, filterValue } = this.state;
         $(document).on('click', '.pro-inner-item', function (e) {
             $('.pro-inner-item').each(function (index) {
                 $(this).removeClass('active');
@@ -255,39 +265,35 @@ class Sidebar extends React.Component {
                                         />
                                     </div>
                                 </div>
-                                {this.state.collapsed ? null : (
-                                    <div className='row mb-2'>
-                                        <div className='col-md-12'>
-                                            <span
-                                                id='menu-search-span'
-                                                className='p-input-icon-left p-input-icon-right'
-                                            >
-                                                <i className='pi pi-search' />
-                                                <InputText
-                                                    ariaLabel={'Wyszukaj menu'}
-                                                    className='p-inputtext-sm'
-                                                    key={'menu-search'}
-                                                    id={'menu-search'}
-                                                    name='filterValue'
-                                                    style={{ width: '100%' }}
-                                                    type='text'
-                                                    placeholder={'Wyszukaj menu'}
-                                                    value={this.state.filterValue}
-                                                    onChange={(e) => this.handleFilter(e.target.value)}
-                                                />
-                                                <i
-                                                    className='pi mdi mdi-close'
-                                                    onClick={(e) => {
-                                                        e.preventDefault();
-                                                        if (this.state.filterValue !== '') {
-                                                            this.handleFilter('');
-                                                        }
-                                                    }}
-                                                />
-                                            </span>
-                                        </div>
+                                <div className='row mb-2' style={collapsed ? { display: 'none' } : {}}>
+                                    <div className='col-md-12'>
+                                        <span id='menu-search-span' className='p-input-icon-left p-input-icon-right'>
+                                            <i className='pi pi-search' />
+                                            <InputText
+                                                ariaLabel={'Wyszukaj menu'}
+                                                className='p-inputtext-sm'
+                                                key='filterValue'
+                                                id='filterValue'
+                                                name='filterValue'
+                                                style={{ width: '100%' }}
+                                                type='text'
+                                                placeholder={'Wyszukaj menu'}
+                                                value={filterValue}
+                                                onChange={(e) => this.handleFilter(e.target.value)}
+                                            />
+                                            <i
+                                                style={filterValue === '' ? { display: 'none' } : {}}
+                                                className='pi mdi mdi-close'
+                                                onClick={(e) => {
+                                                    e.preventDefault();
+                                                    if (filterValue !== '') {
+                                                        this.handleFilter('');
+                                                    }
+                                                }}
+                                            />
+                                        </span>
                                     </div>
-                                )}
+                                </div>
                             </div>
                         </SidebarHeader>
 
